ab0df9eb8872c76ae92b7da61d89dc79555212c3
Sync <68277325+unitysync@users.noreply.github.com>
2024-05-21 03:39:12 +1000

    Update Client
    Removed some unnecessary code on the client side and fixed author in fxmanifest.lua

--- cl_record.lua
+++ cl_record.lua
@@ -1,54 +1,39 @@
 RegisterCommand('record', function()
-  TriggerEvent('sync_recmenu:openui')
-end)
-
-Citizen.CreateThread(function()
-  lib.registerContext({
-    id = 'sync_recmenu',
-    title = 'Recording Menu',
-    options = {
-      {
-        title = 'Rockstar Editor',
-        icon = 'clapperboard',
-        event = 'sync_recmenu:rockstar',
-      },
-      {
-        title = 'Start Recording',
-        icon = 'video',
-        event = 'sync_recmenu:record',
-      },
-      {
-        title = 'Stop & Delete',
-        icon = 'trash',
-        event = 'sync_recmenu:deleterec',
-      },
-      {
-        title = 'Stop & Save',
-        icon = 'floppy-disk',
-        event = 'sync_recmenu:stoprec',
-      }
-    }
-  })
-end)
-
-RegisterNetEvent('sync_recmenu:openui')
-AddEventHandler('sync_recmenu:openui', function()
   lib.showContext('sync_recmenu')
-end)
+end, false)
 
-AddEventHandler('sync_recmenu:record', function()
-  StartRecording(1)
-end)
-
-AddEventHandler('sync_recmenu:rockstar', function()
-  ActivateRockstarEditor()
-  NetworkSessionLeaveSinglePlayer()
-end)
-
-AddEventHandler('sync_recmenu:deleterec', function()
-  StopRecordingAndDiscardClip()
-end)
-
-AddEventHandler('sync_recmenu:stoprec', function()
-  StopRecordingAndSaveClip()
-end)
+lib.registerContext({
+  id = 'sync_recmenu',
+  title = 'Recording Menu',
+  options = {
+    {
+      title = 'Rockstar Editor',
+      icon = 'clapperboard',
+      onSelect = function()
+        ActivateRockstarEditor()
+        NetworkSessionLeaveSinglePlayer()
+      end,
+    },
+    {
+      title = 'Start Recording',
+      icon = 'video',
+      onSelect = function()
+        StartRecording(1)
+      end,
+    },
+    {
+      title = 'Stop & Delete',
+      icon = 'trash',
+      onSelect = function()
+        StopRecordingAndDiscardClip()
+      end,
+    },
+    {
+      title = 'Stop & Save',
+      icon = 'floppy-disk',
+      onSelect = function()
+        StopRecordingAndSaveClip()
+      end,
+    }
+  }
+})
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Sync'
+author 'unitysync'
 description 'Simple recording menu'
 version '1.0.0'
 
